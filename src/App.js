27aa0af15dfd622ff8d4d965dc7b8b6d9aba05b8
dan0703 <dansegura8863@gmail.com>
2024-04-11 21:26:39 -0600

    Administrator menu complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import AdministratorMenu from './Componentes/AdministratorMenu/AdministratorMenu';
+import TopBarMenu from './Componentes/TopBarMenu/TopBarMenu';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div style={{position: "absolute", left: 0, top: 0, bottom: 0, width: "300px", backgroundColor: "#0077b5"}}>
+          <AdministratorMenu/>
+      </div>
+      <div className="right-section" style={{position: "absolute", left: 300, top: 0, bottom: 0, width: "83%", height: "10%", backgroundColor: "#ed0b0b"}}>
+          <TopBarMenu/>
+        </div>
+        
       </header>
     </div>
   );
